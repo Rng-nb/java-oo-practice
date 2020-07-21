953728ea1d5271bec27ca57a147ee4a6a2cd3f46
Rng-nb <18782824515@163.com>
2020-07-21 18:46:14 +0800

    [yafeng Wang] feat: add function userLogin

--- src/com/twu/Main.java
+++ src/com/twu/Main.java
@@ -1,9 +1,13 @@
 package com.twu;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static Manager manager;
+    public static User user;
+    public static HashMap<String, User> userHashMap = new HashMap<String, User>();
 
     public static void main(String[] args) {
         manager = new Manager();
@@ -13,7 +17,7 @@ public class Main {
     }
 
     public static void mainMenu() {
-        String str_title = "欢迎来到热搜排行榜系统，请选择！！！";
+        String str_title = "欢迎来到热搜排行榜系统，请选择！";
         String str_manager = "1. 管理员";
         String str_user = "2. 普通用户";
         String str_exit = "3. 退出";
@@ -28,6 +32,7 @@ public class Main {
                     managerLogin();
                     break;
                 case 2 :
+                    userLogin();
                     break;
                 case 3 :
                     flag_repeat = false;
@@ -54,4 +59,16 @@ public class Main {
         sc_input.close();
     }
 
+    public static void userLogin() {
+        Scanner sc_input = new Scanner(System.in);
+        System.out.println("您好，请输入您的用户名" + '\n' + "用户名:");
+        String input_user_name = sc_input.next();
+        if(userHashMap.containsKey(input_user_name))
+            user = userHashMap.get(input_user_name);
+        else {
+            user = new User();
+            userHashMap.put(input_user_name, user);
+            user.setPersonName(input_user_name);
+        }
+    }
 }
