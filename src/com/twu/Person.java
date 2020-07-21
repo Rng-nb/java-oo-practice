e52801d04ccab08dc8ca3d1f7bc31b889bf87f17
Rng-nb <18782824515@163.com>
2020-07-21 21:50:05 +0800

    [yafeng Wang] feat: add function getHotSearch and insertHotSearch

--- src/com/twu/Person.java
+++ src/com/twu/Person.java
@@ -1,5 +1,8 @@
 package com.twu;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class Person {
     private String personName;
 
@@ -15,12 +18,27 @@ public class Person {
         this.personName = personName;
     }
 
-
-    public void getTHotSearch() {
-
+    public void getTHotSearch(List<HotSearch> hotSearchList) {
+        hotSearchList.sort(new Comparator<HotSearch>() {
+            @Override
+            public int compare(HotSearch o1, HotSearch o2) {
+                if(o1.getTicketHotSearch() > o2.getTicketHotSearch())
+                    return -1;
+                else if(o1.getTicketHotSearch() == o2.getTicketHotSearch())
+                    return 0;
+                else
+                    return 1;
+            }
+        });
+        for (int i = 0; i < hotSearchList.size(); ++i) {
+            HotSearch output_tmp = hotSearchList.get(i);
+            System.out.println(i + "  " + output_tmp.getNameHotSearch() + "  " + output_tmp.getTicketHotSearch());
+        }
     }
 
-    public void inserHotSearch() {
-
+    public void insertHotSearch(List<HotSearch> hotSearchList, String hotSearchName) {
+        HotSearch newHotSearch = new HotSearch();
+        newHotSearch.setNameHotSearch(hotSearchName);
+        hotSearchList.add(newHotSearch);
     }
 }
