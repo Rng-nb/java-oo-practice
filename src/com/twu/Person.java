728d9c9797a8b966eb6e61693885b947e86ac642
Rng-nb <18782824515@163.com>
2020-07-21 16:44:09 +0800

    [yafeng Wang] feat: init the Person.java

--- src/com/twu/Person.java
+++ src/com/twu/Person.java
@@ -1,4 +1,30 @@
 package com.twu;
 
 public class Person {
+    private String personName;
+    private int ticketNumber;
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public int getTicketNumber() {
+        return ticketNumber;
+    }
+
+    public void setTicketNumber(int ticketNumber) {
+        this.ticketNumber = ticketNumber;
+    }
+
+    public void getTHotSearch() {
+
+    }
+
+    public void inserHotSearch() {
+        
+    }
 }
