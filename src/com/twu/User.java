efc6a94cd6979a653d361b08bc507b5622463e62
Rng-nb <18782824515@163.com>
2020-07-21 21:48:55 +0800

    [yafeng Wang] feat: add function giveTicket

--- src/com/twu/User.java
+++ src/com/twu/User.java
@@ -1,5 +1,8 @@
 package com.twu;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class User extends Person {
     private int ticketNumber;
 
@@ -10,12 +13,18 @@ public class User extends Person {
     public int getTicketNumber() {
         return ticketNumber;
     }
+
     public void setTicketNumber(int ticketNumber) {
         this.ticketNumber = ticketNumber;
     }
 
-    public void giveTicKetToHotSearch() {
-
+    public void giveTicKetToHotSearch(HotSearch ticketHotSearch, int ticketNumberGive) {
+        //更新热度
+        int nowTicketNumber = ticketHotSearch.getTicketHotSearch();
+        if(ticketHotSearch.isSuperHotSearch()) {
+            ticketNumberGive = 2 * ticketNumberGive;
+        }
+        ticketHotSearch.setTicketHotSearch(nowTicketNumber + ticketNumberGive);
     }
 
     public void buyHotSearch() {
